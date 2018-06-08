b4350ef2c7a98b8fc7236bef6c4084b39da59891
David Szabo <david.szabo97@gmail.com>
2018-06-08 17:07:04 +0200

    Force refresh when pre-cache changes

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,23 @@
 import { h } from 'preact';
 import Guesser from './guesser';
 
+if (typeof window !== 'undefined') {
+	const navigator = window.navigator;
+	if (typeof navigator !== 'undefined') {
+		if ('serviceWorker' in navigator) {
+			// eslint-disable-next-line compat/compat
+			const serviceWorker = navigator.serviceWorker;
+			if (serviceWorker.controller) {
+				serviceWorker.controller.onstatechange = function(event) {
+					if (event.target.state === 'redundant') {
+						window.location.reload();
+					}
+				};
+			}
+		}
+	}
+}
+
 const App = () => <div id="app"><Guesser /></div>;
 
 export default App;
