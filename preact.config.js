d3296751b04c1e6882d895fa96c431fff68887f8
David Szabo <david.szabo97@gmail.com>
2018-06-08 16:45:41 +0200

    Add sw-precache plugin
    Make sure we only cache what we need.
    
    The original pre-cache settings caused an
    error when it was deployed on Netlify.
    
    Netlify removes the _headers file but the service worker
    tried to cache it so it resulted in an error which prevented
    the service worker from getting updating.

--- preact.config.js
+++ preact.config.js
@@ -1,4 +1,5 @@
 import CopyWebpackPlugin from 'copy-webpack-plugin';
+import preactCliSwPrecachePlugin from 'preact-cli-sw-precache';
 
 export default (config, options, helpers) => {
 	const { plugin } = helpers.getPluginsByName(config, 'HtmlWebpackPlugin')[0] || {};
@@ -7,4 +8,15 @@ export default (config, options, helpers) => {
 	}
 
 	config.plugins.push( new CopyWebpackPlugin([{ context: `${__dirname}/src/public`, from: `**/*` }]) );
+
+	const precacheConfig = {
+		staticFileGlobs: [
+			'build/style.*.css',
+			'build/bundle.*.js',
+			'build/index.html'
+		],
+		stripPrefix: 'build/'
+	};
+
+	return preactCliSwPrecachePlugin(config, precacheConfig);
 };
